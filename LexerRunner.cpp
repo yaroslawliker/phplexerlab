be23abe97c275ddfd0ecf029f67fb28b4c932f3b
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-06-06 19:07:53 +0300

    (feature): added running class

---
+++ LexerRunner.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include "PHPLexer.cpp"
+
+void coutTokens(const std::list<Token>& tokens) {
+
+    for (const auto& token : tokens) {
+
+        if (token.type == TokenType::IDENTIFIER) {
+            std::cout << "Identifier: " << token.value << std::endl;
+        } else if (token.type == TokenType::KEYWORD) {
+            std::cout << "Keyword: " << token.value << std::endl;
+        } else if (token.type == TokenType::INTEGER) {
+            std::cout << "Integer: " << token.value << std::endl;
+        } else if (token.type == TokenType::FLOAT) {
+            std::cout << "Float: " << token.value << std::endl;
+        } else if (token.type == TokenType::STRING) {
+            std::cout << "String: " << token.value << std::endl;
+        } else if (token.type == TokenType::BOOLEAN) {
+            std::cout << "Boolean: " << token.value << std::endl;
+        } else if (token.type == TokenType::NUL) {
+            std::cout << "Null: ";
+        } else {
+            std::cout << "(Map token type with id" << static_cast<int>(token.type) << "): " << token.value << std::endl;
+        }
+    }
+}
+
+
+int main() {
+    
+
+    PHPLexer lexer;
+
+    lexer.setSourceCode("$var1");
+    std::list<Token> tokens = lexer.getTokens();
+
+    coutTokens(tokens);
+
+    return 0;
+}
+
