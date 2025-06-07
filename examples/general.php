246db2f874ab5210da5a12823cfe7eaad5cc1a9f
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-06-08 01:22:00 +0300

    (example): better structure on general.php

--- examples/general.php
+++ examples/general.php
@@ -1,4 +1,5 @@
-$var1 $_var2 $my_var $_
-// This is an example to test all stuff\n
-$var1 if \"hello\" or (true >= false) do 'how are you' && else NULL,
-$_my_var2::123 <=> 123. /* Just testing different stuff */ 45.67 > -0.89 ??;
+// This is an example to test all stuff
+if \"hello\" or (true >= false) do 
+    'how are you' && "looking good" 
+else 
+    NULL, $_my_var2::123 <=> 123. /* Just testing different stuff */ 45.67 > -0.89 ??;
