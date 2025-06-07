1fb99482a3f6e9bd567e8967c84bf9e5eba17abf
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-06-07 23:11:33 +0300

    (example): added NULL to literals

--- literals.php
+++ literals.php
@@ -3,10 +3,12 @@
 123 123.
 45.67 0.89
 true false
+NULL
 
-// Works with symbols before and after
--12 -0.34
-56() 7.8{}
+// Works with symbols before and after the literal
+-12
+(56+7.8)
 !true ~false
-true[] false()
+true|(false)
+(NULL)
 
