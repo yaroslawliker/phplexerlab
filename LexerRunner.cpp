ec9c1c62e07e0194289d3488f0c3779527b16005
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-06-07 15:56:21 +0300

    (feature): added punctuation tests to runer

--- LexerRunner.cpp
+++ LexerRunner.cpp
@@ -23,6 +23,9 @@ void coutTokens(const std::list<Token>& tokens) {
             std::cout << "Null: " << token.value << std::endl;
         } else if (token.type == TokenType::OPERATOR) {
             std::cout << "Operator: " << token.value << std::endl;
+        }
+        else if (token.type == TokenType::PUNCTUATION) {
+            std::cout << "Punctuation: " << token.value << std::endl;
         } else {
             std::cout << "(Map token type with id" << static_cast<int>(token.type) << "): " << token.value << std::endl;
         }
@@ -35,11 +38,14 @@ int main() {
 
     PHPLexer lexer;
 
-    // lexer.setSourceCode("$var1 if \"hello\" or true >= false do 'how are you' && else NULL $_my_var2 123 <=> 123. 45.67 > 0.89 ??");
+    // lexer.setSourceCode("$var1 if \"hello\" or (true >= false) do 'how are you' && else NULL, $_my_var2::123 <=> 123. 45.67 > 0.89 ??;");
     // lexer.setTrace(true); // Enable tracing for debugging
 
     // Testing all operators
-    lexer.setSourceCode("+ = * / % = += -= *= /= %= == === != !== < > <= >= <=> <> && || ! & | ^ ~ << >> .= . ? : ?? @");
+    // lexer.setSourceCode("+ = * / % = += -= *= /= %= == === != !== < > <= >= <=> <> && || ! & | ^ ~ << >> .= . ? : ?? @");
+
+    // Testing all punctuation symbols
+    lexer.setSourceCode("; , :: => -> ?-> ... [ ] { } ( )");
 
 
     std::list<Token> tokens = lexer.getTokens();
